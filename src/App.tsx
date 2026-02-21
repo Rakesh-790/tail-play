384afe3f9a6cb93b0c6bdd6ec4508ee831de1b2a
Rakesh_790 <brakeshroshan098@gmail.com>
2026-02-21 10:57:09 +0530

    initialize project with React, Vite, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='grid grid-cols-2 h-screen bg-red-500 '>
+    
+      </div>
+    </>
+  )
+}
+
+export default App
